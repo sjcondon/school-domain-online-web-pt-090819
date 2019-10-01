552cd7cea939af27e9c689b4830b15d2b46c32b8
sarah <s.j.condon01@gmail.com>
2019-10-01 00:07:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,5 @@
 # code here!
+require pry
 class School
   attr_accessor :roster
  
@@ -8,6 +9,7 @@ class School
   end
   
   def add_students(students_name, grade)
+    binding.pry
     roster[grade] = []
     roster[grade] << students_name
   end
