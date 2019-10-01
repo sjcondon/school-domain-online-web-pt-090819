cfe6f7c11e32b15a320a983bc2d14e47f1a954ea
sarah <s.j.condon01@gmail.com>
2019-10-01 00:03:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  attr_accessor :roster
+ 
+  def initialize(name)
+    @name=name
+    @roster ={}
+  end
+  
+  def add_students(students_name, grade)
+    roster[grade] = []
+    roster[grade] << students_name
+  end
+  
+end
